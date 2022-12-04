ab0cc586a97f2567306ebd4acc819ffa2d40971f
Felix von Poblotzki <xilaew@gmail.com>
2022-12-04 22:05:51 +0100

    day 01 solved

--- day01_calorieCounting_02.py
+++ day01_calorieCounting_02.py
@@ -1,27 +1,19 @@
 import fileinput
 
-max = 0
-current = 0
-noElves = 0
-noLines = 0
+calories=list()
+current=0
 for line in fileinput.input():
     line = line.strip()
     lineval=0
-    noLines += 1
     if line == "":
-        noElves +=1
-        print ("{noElves} Elf: {current}".format(noElves=noElves,current=current))
-        if max<current:
-            max=current
+        calories.append(current)
         current=0
     else:
         lineval = int(line)
         current += lineval
-    print("l {noLines}, elf {elf}, {line}, {lineval}, current = {current}".format(noLines=noLines,elf=noElves,line=line,lineval=lineval,current=current))
-noElves +=1
-print ("{noLines} {noElves} Elf: {current}".format(noLines=noLines,noElves=noElves,current=current))
-if max<current:
-    max=current
-current=0
+calories.append(current)
 
-print(max)    
+print(calories)
+calories.sort(reverse=True)
+print(calories)
+print(f"{calories[0]} + {calories[1]} + {calories[2]} = {calories[0]+calories[1]+calories[2]}")
--- runSolutions.bat
+++ runSolutions.bat
@@ -1,1 +1,2 @@
-python day01_calorieCounting.py <input_day01.txt
+python day01_calorieCounting_01.py <input_day01.txt
+python day01_calorieCounting_02.py <input_day01.txt
