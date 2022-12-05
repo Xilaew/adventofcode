7cd01e5d0b9deaf0b40df67d5529b9844a1b7b6e
Felix von Poblotzki <xilaew@gmail.com>
2022-12-05 08:17:06 +0100

    day05 first part

--- runSolutions.bat
+++ runSolutions.bat
@@ -7,5 +7,7 @@ rem python day02_rockPaperScissors_02.py <input_day02.txt
 rem python day03_rucksackReorganization_01.py <input_day03.txt
 rem python day03_rucksackReorganization_02.py <input_day03.txt
 
-python day04_campCleanup_01.py <input_day04.txt
-python day04_campCleanup_02.py <input_day04.txt
+rem python day04_campCleanup_01.py <input_day04.txt
+rem python day04_campCleanup_02.py <input_day04.txt
+
+python day05_supplyStacks_01.py <input_day05.txt
