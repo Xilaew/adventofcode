2b8df5defd2a0611f3d2ddaef355d16f8b1062ef
Felix von Poblotzki <xilaew@gmail.com>
2022-12-06 06:13:43 +0100

    solved day6

--- day06_01.py
+++ day06_01.py
@@ -2,3 +2,10 @@ import fileinput
 
 for line in fileinput.input():
     line = line.strip()
+    print(line)
+    for i in range(14,line.__len__()-14):
+        s = set(line[i-14:i])
+        if s.__len__()==14:
+            print(i)
+            break
+        print(f"{s}  {s.__len__()}")
