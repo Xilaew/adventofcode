f3a1387f0330d3d6c52d4ab6f914a111767723fc
Felix von Poblotzki <xilaew@gmail.com>
2022-12-16 01:42:42 +0100

    Solved Day12 (python again)

---
+++ day12_hillClimbingAlgorithm_01.py
@@ -0,0 +1,57 @@
+import fileinput
+import numpy
+
+
+def bfsStep(point):
+    # identify all points possibly reachable
+    pointUp = (point[0]-1, point[1])
+    pointDown = (point[0]+1, point[1])
+    pointLeft = (point[0], point[1]-1)
+    pointRight = (point[0], point[1]+1)
+    points = [pointUp, pointDown, pointLeft, pointRight]
+    nextPoints = []
+    for p in points:
+        # stay within the grid
+        if p[0] < 0 or p[0] >= landscape.shape[0] or p[1] < 0 or p[1] >= landscape.shape[1]:
+            continue
+        # possible to move if the next point is not more than one higher than your current point
+        if landscape[p]-landscape[point] <= 1 :
+            # examination only usefull if there was not already a shorter path to this point
+            if distance[point] < distance[p] - 1:
+                nextPoints.append(p)
+                distance[p] = distance[point] + 1
+            if p == end:
+                print(f"found the exit at {end} with shortest path of only {distance[end]}")
+                exit()
+    return nextPoints
+
+
+start = None
+end = None
+y = 0
+landscape = []
+for line in [l.strip() for l in fileinput.input()]:
+    xS = line.find("S")
+    xE = line.find("E")
+    if xS >= 0:
+        start = (y, xS)
+        line = line.replace("S", "a")
+    if xE >= 0:
+        end = (y, xE)
+        line = line.replace("E", "z")
+    landscape.append([ord(x) for x in line])
+    y += 1
+landscape = numpy.array(landscape)
+print(landscape)
+distance = numpy.full(landscape.shape, numpy.iinfo(
+    numpy.uint32).max, numpy.uint32)
+distance[start] = 0
+print(distance)
+# bfs
+nextPoint = start
+search = []
+while nextPoint != None:
+    search += bfsStep(nextPoint)
+    print(search)
+    nextPoint = search.pop(0)
+
